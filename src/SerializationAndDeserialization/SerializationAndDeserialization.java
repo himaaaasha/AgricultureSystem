68dd40ba06605a81dd9d8a9d5d2cc4569f8f1de5
Himasha Harinda <himashaharinda@hotmail.com>
2017-08-03 21:22:49 +0530

    serialization package

--- src/SerializationAndDeserialization/SerializationAndDeserialization.java
+++ src/SerializationAndDeserialization/SerializationAndDeserialization.java
@@ -5,28 +5,10 @@
  */
 package SerializationAndDeserialization;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
 /**
  *
  * @author Himasha Harinda
  */
 public class SerializationAndDeserialization {
-    public static void Serialization( String fileName, Object object) throws IOException{
-        try (FileOutputStream fileoutputstream = new FileOutputStream(fileName)){
-            ObjectOutputStream objectOS = new ObjectOutputStream(fileoutputstream);
-            objectOS.writeObject(object);
-            objectOS.flush();
-        }
-}     
-   //read data deserialize
-    public static Object Deserialization(String fileName) throws IOException, ClassNotFoundException {
-        FileInputStream fileinputstream = new FileInputStream(fileName);
-        ObjectInputStream objectIS = new ObjectInputStream(fileinputstream);
-        return objectIS.readObject();
-}    
+    
 }
